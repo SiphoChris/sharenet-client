1ec29381bfaebf43e2b95033c50ad8ac1530fa46
Sipho Nkebe <siphonkebe@gmail.com>
2025-03-20 11:52:39 +0200

    ship it

--- src/components/PartnersComp.vue
+++ src/components/PartnersComp.vue
@@ -8,13 +8,13 @@
 
 <script setup>
 const partnerImages = [
-  { name: 'image1', path: '../../public/images/partner-1.png' },
-  { name: 'image2', path: '../../public/images/partner-2.png' },
-  { name: 'image3', path: '../../public/images/partner-3.png' },
-  { name: 'image4', path: '../../public/images/partner-4.png' },
-  { name: 'image5', path: '../../public/images/partner-5.png' },
-  { name: 'image6', path: '../../public/images/partner-6.png' },
-  { name: 'image7', path: '../../public/images/partner-7.png' },
-  { name: 'image8', path: '../../public/images/partner-8.png' },
+  { name: 'image1', path: 'images/partner-1.png' },
+  { name: 'image2', path: 'images/partner-2.png' },
+  { name: 'image3', path: 'images/partner-3.png' },
+  { name: 'image4', path: 'images/partner-4.png' },
+  { name: 'image5', path: 'images/partner-5.png' },
+  { name: 'image6', path: 'images/partner-6.png' },
+  { name: 'image7', path: 'images/partner-7.png' },
+  { name: 'image8', path: 'images/partner-8.png' },
 ]
 </script>
